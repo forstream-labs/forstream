287e3dca35aa97647d80f1f72f3cf69547c23f87
Sandro Simas <sandro.dacosta@uala.com.ar>
2020-06-29 21:57:09 -0300

    Release forstream-0.0.17

---
+++ src/services/stream-providers/rtmp.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const {constants} = require('@forstream/models');
+
+exports.createLiveStream = async (connectedChannel) => {
+  return {
+    broadcast_id: null,
+    stream_url: `${connectedChannel.target.metadata.rtmp_url}/${connectedChannel.target.metadata.stream_key}`,
+    stream_status: constants.streamStatus.READY,
+  };
+};
+
+exports.startLiveStream = async (liveStream, providerStream) => {
+  providerStream.set({stream_status: constants.streamStatus.LIVE, messages: []});
+};
+
+exports.endLiveStream = async (providerStream) => {
+  providerStream.set({stream_status: constants.streamStatus.COMPLETE});
+};
+
+exports.isActiveLiveStream = async () => true;
